7ecadc5c5838f8d577b7b3de3a0ef84e5be225f3
xiangbaoyan <271190187@qq.com>
2014-01-01 21:36:18 +0800

    增加保存的文件的功能

--- test.js
+++ test.js
@@ -1,23 +1,30 @@
 var http = require("http");
-
+var fs = require("fs");
 var url = "http://symfony.com/doc/current/index.html";
-function download(url,callback){
+var destName = "C:/Users/Administrator/Desktop/test/ttt.html";
 
+
+
+function getPage(url,callback){
     http.get(url,function(res){
-        var data = "";
+        var data = null;
         res.on('data',function(chunk){
             data += chunk;
         });
         res.on('end',function(){
             callback(data);
         });
-    }).on("error",function(){
+    }).on('error',function(){
             callback(null);
         });
 }
 
 
 
-download(url,function(data){
-    console.log(data);
-});
+    getPage(url,function(data){
+        fs.writeFile(destName,data,function(err){
+            if(err) throw err;
+            console.log("保存了这份文件");
+        });
+    });
+
